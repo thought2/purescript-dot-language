6deb09cafc9cdacadb2c852940d795096cc77bf0
Michael Bock <me@thought2.de>
2022-02-25 10:18:17 +0100

    Init

---
+++ examples/Example1.purs
@@ -0,0 +1,23 @@
+module Example1 where
+
+import Prelude
+import Data.Maybe (Maybe(..))
+import Language.Dot as D
+
+graph :: D.Graph
+graph =
+  D.Graph
+    { strict: true
+    , type: D.directed_
+    , id: Just $ D.Id "my-graph"
+    , stmts:
+        [ D.nodeStmt
+            $ D.NodeStmt
+                { id: D.NodeId { id: D.Id "node1", port: Nothing }
+                , attrs:
+                    [ D.label "Foo"
+                    , D.shape D.box_
+                    ]
+                }
+        ]
+    }
